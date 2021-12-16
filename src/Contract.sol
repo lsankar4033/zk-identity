3a958ab666d229516ca1b4515afc4f0127b13561
lsankar4033 <lsankar4033@gmail.com>
2021-12-15 18:23:32 -0800

    Mock mint method on contract

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,11 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.10;
 
-contract Contract {}
+contract ProofOfDfWinner // is Verifier, ERC721Mintable
+{
+    function mint(bytes proof, bytes nullifier) external {
+        // verify proof
+        // check that nullifier hasn't been used
+        // mint NFT
+    }
+}
